dae48edd07be84f21a407a1baacc137168be2c04
Justin Shilling <loveofmusic25@hotmail.com>
2020-09-20 12:58:48 -0400

    auth config changes

--- src/plugins/auth0.js
+++ src/plugins/auth0.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import router from "../router";
-import { domain, clientId } from "../../auth_config.json";
 import { Auth0Plugin } from "../auth/authWrapper";
+
+const domain =
+  process.env.NODE_ENV == "development"
+    ? "dev-p97jtvjf.us.auth0.com"
+    : "prod-p97jtvjf.us.auth0.com";
+
+const clientId =
+  process.env.NODE_ENV == "development"
+    ? "X2VRKYB7gfxivS402x8klJBpdKaVIRqZ"
+    : "VptEVxxPnKNRcfFv8S8Os6XL9C16bMlA";
 
 Vue.use(Auth0Plugin, {
   domain,
